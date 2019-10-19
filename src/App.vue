e0da90a4643a1c6334611674b5cd58a69a07e7c7
Calum Chisholm <calum.chisholm@hotmail.com>
2019-10-19 11:54:54 +0100

    setup and display of data working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="html">
+ <crypto-chart :cryptoStats="this.cryptoStats"></crypto-chart>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CryptoChart from '@/components/CryptoChart'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      cryptoStats: null
+    }
+  },
+  mounted(){
+    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d')
+    .then(res => res.json())
+    .then(data => this.cryptoStats = data);
+  },
   components: {
-    HelloWorld
+    'crypto-chart': CryptoChart
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
+
+name: 'crypto-chart',
+props: ['cryptoStats']
---
+++ src/components/CryptoChart.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <!-- <GChart
+    type="PieChart"
+    :data='formattedData'
+    :options='chartOptions'
+    class="chart"/> -->
+    <ul>
+      <li v-for="item in cryptoStats">{{item}}</li>
+    </ul>
+</template>
+
+<script>
+import { GChart } from 'vue-google-charts'
+
+export default {
+  name: 'crypto-chart',
+  props: ['cryptoStats'],
+  components: {
+
+  }
+}
+
+
+</script>
+
+<style lang="css" scoped>
+</style>
